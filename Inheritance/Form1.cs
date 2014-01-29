45603b32fe8b15af4976a8f866b10a1976b2ec76
andrewjhinger <andrewjhinger@gmail.com>
2014-01-29 09:34:17 -0500

    jh

---
+++ Inheritance/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inheritance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Ellipse ellipse = new Ellipse();
+            Rectangle rectangle = new Rectangle();
+        }
+
+        public class Shape
+        {
+            public Shape()
+            {
+                Console.WriteLine("Shape constructor");
+            }
+        }
+
+        public class Ellipse : Shape
+        {
+            public Ellipse()
+            {
+                Console.WriteLine("Ellipse constructor");
+            }
+        }
+
+        public class Rectangle : Shape
+        {
+            public Rectangle()
+            {
+                Console.WriteLine("Rectangle constructor");
+            }
+        }
+    }
+}
